6681a0532172964ead43fedfd0078a5c4ac77630
git clone git@testyourprojects.biz:/home/matrix2016/git/mucademy2021 <santanumukherjee901@gmail.com>
2023-08-16 18:24:31 +0530

    server with mysql and node.js

--- app.js
+++ app.js
@@ -1,39 +1,115 @@
-const express = require('express');
-const mysql = require('mysql');
 
-// Create MySQL connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'matrix@2022',
-    port: 3306, // Change this to your MySQL port (usually 3306)
+
+
+
+
+const mysql= require("mysql");
+const express = require("express");
+const con = mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"test"
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log('MySQL connection error:', err);
-        throw err;
-    }
-    console.log('MySql Connected...');
-});
+con.connect((err)=>{
+  if(err)
+  {
+    console.warn("not connect");
+  }else{
+    console.warn("connected!!!");
+  }
+})  
 
 const app = express();
 
-// Create DB route
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE IF NOT EXISTS nodemysql';
-    db.query(sql, (err, result) => {
-        if (err) {
-            console.error('Error creating database:', err);
-            res.status(500).send('Error creating database.');
-            return;
-        }
+//create db
+app.get('/createdb', (req,res) => {
+    let sql = 'CREATE DATABASE test';
+    con.query(sql, (err,result) => {
+          if(err)  throw err;
+          console.log(result);
+          res.send("Database created...");
+    })
+}) 
+
+//create tables
+app.get('/createpoststable', (req,res) => {
+    let sql = 'CREATE TABLE IF NOT EXISTS posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+    con.query(sql, (err,result) => {
+        if(err)  throw err;
         console.log(result);
-        res.send('Database created or already exists.');
+        res.send('Posts table created...');
     });
 });
 
-const port = 3000; // This is the port for your Express server
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+//Insert post
+app.get('/addpost1', (req,res) => {
+    let post = {title: 'Post One', body: 'This is post number one'};
+    let sql = 'INSERT INTO posts SET ?';
+    let query = con.query(sql, post, (err,result) => {
+        if(err)  throw err;
+        console.log(result);
+        res.send('Post 1 added...');
+    })
+}) 
+
+//Insert post 2
+app.get('/addpost2', (req,res) => {
+    let post = {title: 'Post Two', body: 'This is post number two'};
+    let sql = 'INSERT INTO posts SET ?';
+    let query = con.query(sql, post, (err,result) => {
+        if(err)  throw err;
+        console.log(result);
+        res.send('Post 2 added...');
+    });
+});
+
+//Select posts
+
+app.get('/getposts', (req,res) => {
+   
+    let sql = 'SELECT * FROM posts';
+    let query = con.query(sql, (err,results) => {
+        if(err)  throw err;
+        console.log(results);
+        res.send('Posts fetched...');
+    });
+});
+
+//Select single posts
+app.get('/getpost/:id', (req,res) => {
+   
+    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+    let query = con.query(sql, (err,result) => {
+        if(err)  throw err;
+        console.log(result);
+        res.send('Posts fetched...');
+    });
+});
+
+//Update post
+app.get('/updatepost/:id', (req,res) => {
+    let newTitle = 'Updated Title';
+    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+    let query = con.query(sql, (err,result) => {
+        if(err)  throw err;
+        console.log(result);
+        res.send('Post updated...');
+    });
+});
+
+//Delete post 
+app.get('/deletepost/:id', (req,res) => {
+    let newTitle = 'Updated Title';
+    let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
+    let query = con.query(sql, (err,result) => {
+        if(err)  throw err;
+        console.log(result);
+        res.send('Post deleted...');
+    });
+}); 
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
 });
